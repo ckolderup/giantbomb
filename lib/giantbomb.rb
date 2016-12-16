65be8a17bebdfc90bf044cbc2846afd6f0fc9e44
Casey Kolderup <ckolderup@users.noreply.github.com>
2016-12-16 14:11:24 -0800

    version got moved to version.rb

--- lib/giantbomb.rb
+++ lib/giantbomb.rb
@@ -8,7 +8,3 @@ end
 ["game","company","character","franchise","concept","object","location","person","video"].each do |inc|
   require File.join(File.dirname(__FILE__), "giantbomb", inc)
 end
-
-module GiantBomb
-  VERSION = "0.5.3"
-end
